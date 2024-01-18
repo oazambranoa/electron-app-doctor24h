6430fd6d7fee4fd19152a9c1f097048f47aeebac
oazambranoa <oazambranoa@unal.edu.co>
2024-01-18 04:21:55 -0500

    Se ajusta la pantalla de inicio

--- frontend/doctor24h/public/electron.js
+++ frontend/doctor24h/public/electron.js
@@ -2,8 +2,8 @@ import { app, BrowserWindow } from 'electron';
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1000,
+        height: 800
     })
     win.loadURL('http://localhost:6969')
 }
